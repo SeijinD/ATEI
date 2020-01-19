52ad8d09422013c952954b55b2c7837dc83eb7a9
George Kara <seijindemon@protonmail.com>
2020-01-19 18:49:06 +0200

    add java 3 lab 6

---
+++ Data Structures And Algorithm Analysis with Java (Java 3)/Lab6/ListEmptyException.java
@@ -0,0 +1,9 @@
+package Lab6;
+
+class ListEmptyException extends RuntimeException
+{
+    public ListEmptyException(String err)
+    {
+        super(err);
+    }
+}
---
+++ Data Structures And Algorithm Analysis with Java (Java 3)/Lab6/NoSuchListPosition.java
@@ -0,0 +1,9 @@
+package Lab6;
+
+class NoSuchListPosition extends RuntimeException
+{
+    public NoSuchListPosition(String err)
+    {
+        super(err);
+    }
+}
