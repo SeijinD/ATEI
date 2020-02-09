24b0292e31c38ff5d65e2d656254be264ec58047
George Kara <seijindemon@protonmail.com>
2020-02-09 23:50:41 +0200

    add java 3 lab 10.1

---
+++ Data Structures And Algorithm Analysis with Java (Java 3)/Lab10/Lab10_1/Student.java
@@ -0,0 +1,44 @@
+package Lab10.Lab10_1;
+
+class Student
+{
+    private int am;
+    private String onoma, epitheto;
+
+    public Student(int am, String onoma, String epitheto)
+    {
+        this.am = am;
+        this.onoma = onoma;
+        this.epitheto = epitheto;
+    }
+
+    public void setAM(int am)
+    {
+        this.am = am;
+    }
+
+    public void setOnoma(String onoma)
+    {
+        this.onoma = onoma;
+    }
+
+    public void setEpitheto(String epitheto)
+    {
+        this.epitheto = epitheto;
+    }
+
+    public int getAM()
+    {
+        return am;
+    }
+
+    public String getOnoma()
+    {
+        return onoma;
+    }
+
+    public String getEpitheto()
+    {
+        return epitheto;
+    }
+}
---
+++ Data Structures And Algorithm Analysis with Java (Java 3)/Lab10/Lab10_1/lab10_1.java
@@ -0,0 +1,35 @@
+package Lab10.Lab10_1;
+
+import java.io.*;
+
+class lab10_1
+{
+    public static void main(String[] args)
+    {
+        RamStudent students = new RamStudent();
+
+        try
+        {
+            RandomAccessFile file = new RandomAccessFile("src//Lab10//Lab10_1//lab10_1.dat", "rw");
+
+            for(int i = 0; i < 20; i++)
+            {
+                new RamStudent(12001 + i, "", "").write(file);
+            }
+
+            file.seek(hash(12003));
+
+            RamStudent test = students.read(file);
+            System.out.println("AM: " + test.getAM());
+        }
+        catch(IOException e)
+        {
+            System.out.println(e.toString());
+        }
+    }
+
+    public static int hash(int x)
+    {
+        return 64 * (x - 12001);
+    }
+}
